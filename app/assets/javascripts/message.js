363b31ff5466ed4fcd847e978b66826fac4d3c15
chanken2314 <57705822+chanken2314@users.noreply.github.com>
2019-12-11 12:21:35 +0900

    ajax2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,12 +1,11 @@
-// $(document).on('turbolinks:load', function() {
 $(function() {
   function buildHTML(message){
-     // 画像がアップされないときは<img src = "null">となり
-    //  余計なサムネが表示されることを防ぐ
-    image = (message.image) ? `<img class="lower-message__image" src="${ message.image }">`: "";
+    // 条件式 ? 真の時の値 : 偽の時の値
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
 
-    var html = `<div class="chat__message" >
-                  <div class="caht__message__info">
+    var html = `<div class="chat__message" data-message-id="${message.id}" >
+                  <div class="chat__message__info">
                     <p class="chat__message__user">
                       ${ message.user_name }
                     </p>
@@ -16,47 +15,74 @@ $(function() {
                   </div>
                   <div class="chat__message__text">
                     <p class="chat__message__content">
-                      ${ message.content }
+                      ${ content }
                     </p>
                   </div>
-                  ${image}`
+                  ${img}`
     return html;
   }
-  
 
-  
-    // 1.フォームが送信されたら、イベントが発火するようにする
-  $("#new-message").on("submit", function(e){  
+
+  $("#new_message").on("submit", function(e){  
     e.preventDefault();  
     var formData = new FormData(this);  
     var url = $(this).attr("action")  
 
-    // 2.イベントが発火したときにAjaxを使用して、messages#createが動くようにする  
-    // ajaxを使って、json形式でリクエストを送る。
+
     $.ajax({  
-      url: location.href,  
-      // 現在開いているページのURLを取得するという記述
+      url:  url,  
       type: "POST",  
-      data: { message: {id: message_id} },  
+      data: formData,  
       dataType: "json",  
       processData: false,  
       contentType: false  
     })  
 
-    .done(function(data){  
-      var html = buildHTML(data);  
-      $(".messages").append(html)  
-      $(".lower-message__image").css('max-height','300px');  
-      $(".lower-message__image").css('max-width','300px');  
-      $('#new-message')[0].reset()  
-      // メッセージを送信したとき、メッセージ画面を最下部にスクロールする
-      $('.js-messages').animate({scrollTop: $('.js-messages')[0].scrollHeight});  
+    .done(function(messages){  
+      var html = buildHTML(messages);  
+      $('.chat__messages').val('');
+      $('.chat__messages').append(html)
+      $('.chat__messages').animate({scrollTop: $('.chat__messages')[0].scrollHeight}, 'fast'); 
+      $("#new_message")[0].reset()  
+      $('.form__submit').prop('disabled',false);
     })  
     .fail(function(){  
-      alert("error");  
+      alert('エラーが発生したためメッセージは送信できませんでした。');
     })  
-    .always(function() {  
-      $('.form__submit').prop('disabled',false);  
-    })  
-  })  
+    return false;
+  }) 
+
+
+
+
+  var reloadMessages = function () {
+    
+
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      last_message_id = $('.chat__message:last').data("message-id");
+      
+      
+      $.ajax({
+        url:  'api/messages',
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id} 
+      })
+      .done(function(messages) {
+        
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat__messages').append(insertHTML); 
+        $('.chat__messages').animate({scrollTop: $('.chat__messages').height()})
+      })
+      .fail(function() {
+        alert('更新に失敗しました');
+      });
+    };
+  };
+  setInterval(reloadMessages, 7000); 
 });
+
+
